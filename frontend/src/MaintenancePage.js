b4152d1f8f02933246e3891424413c2fed3da2cc
linmat1 <165717145+linmat1@users.noreply.github.com>
2026-02-01 04:16:14 -0600

    maintenance msg

--- frontend/src/MaintenancePage.js
+++ frontend/src/MaintenancePage.js
@@ -8,7 +8,7 @@ function MaintenancePage() {
         <div className="maintenance-icon">🔧</div>
         <h1>Under Maintenance</h1>
         <p className="maintenance-message">
-          too buggy rn, too many people getting no-shows, working on fixing, should be back in like a few days
+          too buggy rn, too many people getting no-shows, working on fixing, should be back in like TODAY OR TOMORROW
         </p>
         <div className="maintenance-contact">
           <p className="contact-info">+447754666843 on WhatsApp</p>
