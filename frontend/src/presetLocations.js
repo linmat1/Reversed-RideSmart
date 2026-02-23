3b622c3ec28dc05c2721121c7d3577c579787ab3
linmat1 <165717145+linmat1@users.noreply.github.com>
2026-02-23 01:41:06 -0600

    coords change

--- frontend/src/presetLocations.js
+++ frontend/src/presetLocations.js
@@ -7,8 +7,8 @@
  */
 const PRESET_LOCATIONS = [
   { name: "I-House",               lat: 41.7878692,       lng: -87.5908127 },
-  { name: "Cathey/RGGC", lat: 41.7851539,       lng: -87.6011258 },
-  { name: "Campus North",          lat: 41.79470772214912, lng: -87.59838456342511 },
+  { name: "Cathey/RGGC", lat: 41.78502283175025, lng: -87.60087796606041 },
+  { name: "North/Baker",          lat: 41.79470772214912, lng: -87.59838456342511 },
   { name: "Regenstein",            lat: 41.79160594794951, lng: -87.59971312177721 },
   { name: "Woodlawn",              lat: 41.78512450866148, lng: -87.59656658839626 },
   { name: "Trader Joe's",          lat: 41.796424212644666, lng: -87.58835063681816 },
