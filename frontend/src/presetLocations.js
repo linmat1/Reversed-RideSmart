65641c5bcbef7236cc05305f548d47b8d5ff8200
linmat1 <165717145+linmat1@users.noreply.github.com>
2026-03-06 23:33:34 -0600

    smiley face

--- frontend/src/presetLocations.js
+++ frontend/src/presetLocations.js
@@ -10,7 +10,7 @@
 const PRESET_LOCATIONS = [
   { name: "I-House",              lat: 41.7878692,        lng: -87.5908127,        address: "1414 E 59th St, Chicago, IL 60637" },
   { name: "Cathey/RGGC",         lat: 41.78502283175025, lng: -87.60087796606041, address: "6017 S Ellis Ave, Chicago, IL 60637" },
-  { name: "North/Baker",         lat: 41.79470772214912, lng: -87.59838456342511, address: "5500 S University Ave, Chicago, IL 60637" },
+  { name: "North/Baker",         lat: 41.79470772214912, lng: -87.59838456342511, address: "5500 S University Ave, Chicago, IL 60637 ッ" },
   { name: "Regenstein",          lat: 41.792022232216496, lng: -87.59972263872557, address: "1100 E 57th St, Chicago, IL 60637" },
   { name: "Woodlawn",            lat: 41.78512450866148,  lng: -87.59656658839626, address: "1156 E 61st St, Chicago, IL 60637" },
   { name: "Trader Joe's",        lat: 41.796424212644666, lng: -87.58835063681816, address: "1528 E 55th St, Chicago, IL 60615" },
